d40ac63959f92ec214ab71c37e7ef3c7329b7e77
Mariana Nunes <marinun47@gmail.com>
2022-05-20 00:25:39 -0300

    [tarefa-de-casa] updates get by type and by name

--- tarefa-de-casa/src/server.js
+++ tarefa-de-casa/src/server.js
@@ -7,62 +7,59 @@ const app = express();
 app.use(express.json());
 
 app.listen(6000, () => {
-    console.log("Server is on port 6000");
-})
+  console.log("Server is on port 6000");
+});
 
 //TO RETURN ALL POKEMONS:
+
 app.get("/pokemon", (request, response) => {
-    response.status(200).send(pokedex);
-})
+  response.status(200).send(pokedex);
+});
 
 //TO RETURN A POKEMON BY ID:
+
 app.get("/pokemon/:id", (request, response) => {
-    let idRequest = request.params.id;
+  const idRequest = request.params.id;
 
-    let foundPokemon = pokedex.find((pokemon) => pokemon.id == idRequest);
+  const foundPokemon = pokedex.find((pokemon) => pokemon.id == idRequest);
 
-    response.status(200).send(foundPokemon);
-})
+  response.status(200).send(foundPokemon);
+});
 
-//TO RETURN A POKEMON BY NAME
-app.get("/pokemonio/name", (request, response) => {
-    
-    let nameRequest = request.query.name.toLowerCase();
+//TO RETURN A POKEMON BY NAME OR TYPE
 
-    let filteredPokemon = pokedex.filter((pokemon) => pokemon.name.toLowerCase().includes(nameRequest)
-    )
-    response.status(200).send(filteredPokemon)
-})
+app.get("/pokedex/filter", (request, response) => {
+  let payload;
+  const typeRequest = request.query.type;
+  const nameRequest = request.query.name;
 
-//TO RETURN A POKEMON BY TYPE
-app.get("/pokemonio/type", (request, response) => {
+  if (request.query.type) {
+    payload = pokedex.filter((pokemon) => pokemon.type.includes(typeRequest));
+  } else {
+    payload = pokedex.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(nameRequest)
+    );
+  }
 
-    let typeRequest = request.query.type
-    
-    let pokemonFilter = pokedex.filter((pokemon) => pokemon.type.includes(typeRequest.toString()));
+  response.status(200).send(payload);
+});
 
-    response.status(200).send(pokemonFilter)
-})
+//TO CREATE A NEW POKEMON
 
-//TO CREATE A NEW POKEMON deu m
 app.post("/pokemon", (request, response) => {
-    let nameRequest = request.body.name;
-    let typeRequest = request.body.type;
-    let statsRequest = request.body.stats
-        
-    let newPokemon = {
-        id: (pokedex.length) + 1,
-        name: nameRequest,
-        type: [typeRequest.split(" ")],
-        stats: {statsRequest}
-    };
+  const newPokemon = {
+    id: pokedex.length + 1,
+    name: request.body.name,
+    type: request.body.type,
+    stats: request.body.stats,
+  };
 
-    pokedex.push(newPokemon);
+  pokedex.push(newPokemon);
 
-    response.status(201).json([
-        {
-            "mensagem": "New pokemon added.",
-            newPokemon,
-        }
-    ])
-})
+  response.status(201).json([
+    {
+      mensage: "New pokemon added.",
+      newPokemon,
+    },
+  ]);
+});
