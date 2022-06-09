45eef3782f34b85acd4b729bf188455c22b95afa
Nilva <piresnilva@gmail.com>
2022-06-09 20:48:33 -0300

    implantando as rotas filmes

--- tarefa-de-casa/src/serverFilms.js
+++ tarefa-de-casa/src/serverFilms.js
@@ -1,71 +1,30 @@
+const myghibliJson = require("./models/ghiblifilms.json"); //acessa o json dos filmes
+const express = require("express");
+const app = express(); //executar o express
 
-const mayFilmesJson = require('./model/ghiblifilms.json') // JSON  do ghiblifilms
-const express = require('express')// framework express
-const app = express() //executando o express
-
-app.use(express.json()) // body parser 
-
-// porta para chamar
+app.use(express.json()); //Bory parser
+//porta do servidor
 app.listen(3000, () => {
-    console.log("Servidor da tarefa de casa do gliblifilms RODOUUUU")
-})
-//criando uma rota do get
-app.get("/", (request, response) => {
-    response.status(200).json([
-        {
-            "message": "Se chegou até aqui é que o código deu certo. "
-        }
-    ])
-})
-// retornará todos os filmes 
-app.get("/filmes", (request, response) => {
-    response.status(200).send(mayFilmesJson)
-})
-// retornará um filme especifico, pelo id
-app.get("/filmes/buscar/:id", (request, response) => {
-
-    let idRequest = request.params.id//Buscar o id(especifico) 
-
-    let filmeEncontrado = mayFilmesJson.find(filme => filme.id == idRequest) // guardar o filme encontrado
-
-    response.status(200).send(filmeEncontrado)
-})
-
-// retornará diretor
-app.get("/filmes/filtro", (request, response) => {
-
-    let diretorRequest = request.query.titulo.toLocaleLowerCase()
-    console.log(diretorRequest)//Buscar titulo(especifico) 
-
-    let filmeEncontrado = mayFilmesJson.filter(
-        filme => filme.title.toLocaleLowerCase().includes(tituloRequest)
-    )
-
-    response.status(200).send(filmeEncontrado)
-
+  console.log("Não esqueçaaaa!!! O SERVIDOR está na porta 3000");
 });
 
-// incluirá o novo cadastro
-app.post("/filmes", (request, response) => {
+app.get("/", (request, response) => {
+  response.status(200).json([
+    {
+      message:
+        "Se rodou na telinha porque API está funcionando               garotaaaa",
+    },
+  ]);
+});
+// retornar todo os filmes
+app.get("/filmes", (request, response) => {
+  response.status(200).send(myghibliJson);
+});
 
-    let tituloRequest = request.body.title//Guardará o novo título cadastrado
-    let descricaoRequest = request.body.description//Guardará a descrição 
+// retornar pelo id
+app.get("/filmes/buscar/:id", (request, response) => {
+  let idRequest = request.params.id
+  let idEncontrado = myghibliJson.find((filme) => filme.id == idRequest)
 
-    //guardará o novo filme com os seguintes dados id, title e description
-    let novoFilme = {
-        id: (mayFilmesJson.length) + 1,
-        title: tituloRequest,
-        description: descricaoRequest
-    }
-//Enviará o Novo Cadastro para o final do array
-    mayFilmesJson.push(novoFilme)
-
-    response.status(201).json(
-        [{
-            "mensagem": "Cadastro do filme foi realizado com SUCESSOOOOOOO",
-            novoFilme
-        }]
-
-    )
-
+  response.status(200).send(idEncontrado)
 })
